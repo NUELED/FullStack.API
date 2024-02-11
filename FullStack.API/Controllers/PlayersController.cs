51c7abaa8e17eefd2116f089809d9b35ff7a8028
ee <emmanuel.edem@fmdqgroup.com>
2024-02-11 05:15:38 +0100

    completed proj.

--- FullStack.API/Controllers/PlayersController.cs
+++ FullStack.API/Controllers/PlayersController.cs
@@ -33,6 +33,7 @@ namespace FullStack.API.Controllers
 
 
         [HttpPost]
+        [Route("create-player")]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status201Created)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
@@ -45,5 +46,64 @@ namespace FullStack.API.Controllers
 
             return Ok(playerRequest);   
         }
+
+        [HttpGet]
+        [Route("{id:Guid}")]
+        public async Task<IActionResult> GetPlayer([FromRoute] Guid id)
+        {
+            var player = await _context.players.FirstOrDefaultAsync(u => u.id == id);
+
+            if (player == null)
+            { 
+                return NotFound();
+            }
+
+            return Ok(player);  
+        }
+
+        [HttpPut]
+        [Route("{id:Guid}")]
+        public async Task<IActionResult> UpdatePlayer([FromRoute] Guid id, Player updatePlayerRequest)
+        {
+            var player = await _context.players.FirstOrDefaultAsync(u => u.id == id);
+
+            if (player == null)
+            {
+                return NotFound();
+            }
+
+            player.Name = updatePlayerRequest.Name; 
+            player.Email = updatePlayerRequest.Email;   
+            player.Salary = updatePlayerRequest.Salary; 
+            player.Phone = updatePlayerRequest.Phone;
+            player.Club = updatePlayerRequest.Club;
+
+            _context.players.Update(player);
+            await _context.SaveChangesAsync();
+            return Ok(player);
+
+        }
+
+
+        [HttpDelete]
+        [Route("{id:Guid}")]
+        public async Task<IActionResult> DeletePlayer([FromRoute] Guid id)
+        {
+            var player = await _context.players.FirstOrDefaultAsync(u => u.id == id);
+
+            if (player == null)
+            {
+                return NotFound();
+            }
+
+            _context.players.Remove(player);
+            await _context.SaveChangesAsync();
+            return Ok(player);
+
+        }
+
+
+
+
     }
 }
--- FullStack.API/Program.cs
+++ FullStack.API/Program.cs
@@ -6,12 +6,13 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 
-var dbHost =  Environment.GetEnvironmentVariable("DB_HOST");                          //"DESKTOP-0FRMSIG";  
-var dbName =  Environment.GetEnvironmentVariable("DB_NAME");                         //"FullStackDb";
-var dbPassword =  Environment.GetEnvironmentVariable("DB_MSSQL_SA_PASSWORD");       //"password1";
-var connectionString = $"Server={dbHost};Database={dbName};User ID=sa;Password={dbPassword}";//Trusted_Connection=True;Encrypt=False;This is for the Db.I took it out
+//var dbHost =  Environment.GetEnvironmentVariable("DB_HOST");                          //"DESKTOP-0FRMSIG";  
+//var dbName =  Environment.GetEnvironmentVariable("DB_NAME");                         //"FullStackDb";
+//var dbPassword =  Environment.GetEnvironmentVariable("DB_MSSQL_SA_PASSWORD");       //"password1";
+//var connectionString = $"Server={dbHost};Database={dbName};User ID=sa;Password={dbPassword}";//Trusted_Connection=True;Encrypt=False;This is for the Db.I took it out
 
-builder.Services.AddDbContext<FullstackDbContext>(opt => opt.UseSqlServer(connectionString));
+//builder.Services.AddDbContext<FullstackDbContext>(opt => opt.UseSqlServer(connectionString));
+builder.Services.AddDbContext<FullstackDbContext>(opt => opt.UseSqlServer(builder.Configuration.GetConnectionString("FullstackConnectionString")));
 
 
 //builder.Services.AddDbContext<FullstackDbContext>(opt =>opt.UseSqlServer(builder.Configuration.GetConnectionString("FullstackConnectionString")));
